f83badf5a348093dff431770ed6e1c4cb92878ef
Chase杨念 <65386393+Little-YangNian@users.noreply.github.com>
2021-02-07 22:03:42 +0800

    Update Maker.py

--- docs/Maker.py
+++ docs/Maker.py
@@ -13,7 +13,7 @@ def maker():
         with open(f"./docs/{oname+'-'+pname}.md",mode="w",encoding="utf-8") as f:
             md = f"# 项目名 {pname}  \n## 拥有者 [{oname}]({ourl})  \n## 项目地址 [{repo}]({purl})  \n{cst}"      
             f.write(md)
-        with open("./docs/Readme.md",mode="a") as f:
+        with open("./docs/Readme.md",mode="a+") as f:
             if repo not in f.read():
                 f.write(f"[{repo}](/{oname+'-'+pname})  \n")
                 
