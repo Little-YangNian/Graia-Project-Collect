e832c49b606911a766f89b497cae623fb0ed49bb
Chase杨念 <65386393+Little-YangNian@users.noreply.github.com>
2021-02-07 20:52:42 +0800

    Update Maker.py

--- docs/Maker.py
+++ docs/Maker.py
@@ -11,9 +11,9 @@ def maker():
         repo = i["Repo"]
         ourl = i["Owner_Url"]
         cst = i["Custom_Markdown"]
-        path = pathlib.Path(repo)
+        path = pathlib.Path("./docs/"+repo)
         if not path.is_dir():
-            os.system(f"mkdir {repo}")
+            os.system(f"mkdir ./docs/{repo}")
         with open(f"./docs/{repo}/index.md",mode="w",encoding="utf-8") as f:
             md = f"# 项目名 {pname}  \n## 拥有者 [{oname}]({ourl})  \n## 项目地址 [{repo}]({purl})  \n{cst}"      
             f.write(md)
