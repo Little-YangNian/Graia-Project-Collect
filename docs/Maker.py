17cecdda8f7cd0b8a7bb141ae6cb8089f682e0b8
Chase杨念 <65386393+Little-YangNian@users.noreply.github.com>
2021-02-07 20:39:00 +0800

    Update Maker.py

--- docs/Maker.py
+++ docs/Maker.py
@@ -10,10 +10,8 @@ def maker():
         repo = i["Repo"]
         ourl = i["Owner_Url"]
         cst = i["Custom_Markdown"]
-        for i in os.listdir("./docs"):
-            q = 1
-            q += 1
-        with open(f"./docs/{q+1}.md",mode="w",encoding="utf-8") as f:
+        os.mkdir(f"./docs/{repo})
+        with open(f"./docs/{repo}/index.md",mode="w",encoding="utf-8") as f:
             md = f"# 项目名 {pname}  \n## 拥有者 [{oname}]({ourl})  \n## 项目地址 [{repo}]({purl})  \n{cst}"      
             f.write(md)
 
